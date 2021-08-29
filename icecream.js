6f46605372f52cce69684712dee75b72ae4e6f71
Adriana <adriana.calvomatos@gmail.com>
2021-08-29 16:30:31 +0000

    add lesson files

---
+++ icecream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry',
+];
+
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+console.log(iceCreamFlavours.length);
+
+console.log(iceCreamFlavours);
