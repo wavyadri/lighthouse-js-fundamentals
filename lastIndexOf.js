455765d1aac12c11fede7131c02a99aa8838ba1f
Adriana <adriana.calvomatos@gmail.com>
2021-08-29 17:21:04 +0000

    add lesson files

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,14 +1,21 @@
+// would like to come back and review this to code a cleaner solution
+
 const lastIndexOf = (array, value) => {
   let lastOcurr;
+  // if array is empty or undef
   if (array.length <= 0) {
     return -1;
   } else {
     // loop backwards so we don't have to deal with duplicates
     for (let i = array.length - 1; i >= 0; i--) {
+      // if we find a match, return
+      // this will be the last occurance of it in the array
+      // regardless of values before it
       if (array[i] === value) {
         return (lastOcurr = i);
       }
     }
+    // if after looping through the whole array there is no match
     if (array[0] !== value) {
       return -1;
     }
