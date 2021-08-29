d7d90d2874ccdc0822773db85a52d2906d5445d1
Adriana <adriana.calvomatos@gmail.com>
2021-08-29 16:31:28 +0000

    add lesson files

--- packing-list.js
+++ packing-list.js
@@ -13,3 +13,11 @@ console.log('Kitchen stuff to pack:');
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+// while loop
+console.log('Kitchen stuff to pack:');
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
