ed6eed9ea642808deef7e9a75c644bcb25eedd31
Adriana <adriana.calvomatos@gmail.com>
2021-09-05 10:34:52 +0000

    update files

---
+++ test.js
@@ -0,0 +1,22 @@
+const blocksAway = function (directions) {
+  // Put your solution here
+};
+
+console.log(blocksAway(['right', 2, 'left', 3, 'left', 1]));
+console.log(
+  blocksAway([
+    'left',
+    1,
+    'right',
+    1,
+    'left',
+    1,
+    'right',
+    1,
+    'left',
+    1,
+    'right',
+    1,
+  ])
+);
+console.log(blocksAway(['left', 3, 'right', 1, 'right', 3, 'right', 1]));
